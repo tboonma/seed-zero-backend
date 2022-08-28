a31e2d84c481588e617fedd6492ee788a91ca248
vjumpkung <vjumpkung@gmail.com>
2022-08-28 15:44:28 +0700

    finished work

--- main.py
+++ main.py
@@ -9,9 +9,9 @@ from models import Item
 NAME = os.getenv('NAME', default="World")
 
 app = FastAPI()
-
+ 
 # TODO: 1.) Add endpoint as a top-level page.
-@app.get("")
+@app.get("/")
 def index():
     return {"message": f"Hello {NAME}"}
 
@@ -19,21 +19,29 @@ def index():
 @app.get("/calculator/add")
 def adder(first_number: int, second_number: int):
     # TODO: Start here
-    return {"first_number": first_number, "second_number": second_number, "result": 0}
+    return {"first_number": first_number, "second_number": second_number, "result": first_number+second_number}
 
 @app.get("/calculator/substract")
 def subtractor(first_number: int, second_number: int):
     # TODO: Start here
-    return {"first_number": first_number, "second_number": second_number, "result": 0}
+    return {"first_number": first_number, "second_number": second_number, "result": first_number-second_number}
 
 # TODO: (Optional) @app.get("/calculator/multiply")
+@app.get("/calculator/multiply")
+def multiplier(first_number: int, second_number: int):
+    return {"first_number": first_number, "second_number": second_number, "result": first_number*second_number}
 
 # TODO: (Optional) @app.get("/calculator/divide")
-
+@app.get("/calculator/divide")
+def diviver(first_number: int,second_number: int):
+    if(second_number != 0):
+        return {"first_number": first_number, "second_number": second_number, "result": first_number/second_number}
+    else:
+        return {"first_number": first_number, "second_number": second_number, "result": "zero division error"}
 
 
 # TODO: 3.) Enter a path scheme that accept userId as a path parameter `userId`.
-@app.get("")
+@app.get("/userId")
 def usersInfo(userId: int):
     if userId != 9997:
         raise HTTPException(status_code=404, detail="UserId not found.")
@@ -51,9 +59,9 @@ def usersInfo(userId: int):
 #   "name": "Book",
 #   "price": 123,
 # }
-# @app.????(???)
+@app.post("/item/create")
 def itemCreator(item: Item):
-    # TODO: Print body's name and price to console.
-    return item.name, item.price
+    print(f"name is {item.name} , price is {item.price}")
+    return item.name,item.price
 
 
