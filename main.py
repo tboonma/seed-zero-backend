dd6bca201150b551d67864dd79a060975ff6ed1a
VjumpKunG <vjumpkung@gmail.com>
2023-10-02 22:26:18 +0700

    improving api and docs

--- main.py
+++ main.py
@@ -1,50 +1,71 @@
 from http.client import HTTPException
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, status
 from dotenv import load_dotenv
 import os
+
 load_dotenv()
 
-from models import Item
+from models import Item, Message, CalculatorResponse, UserInfo
 
-NAME = os.getenv('NAME', default="World")
+NAME = os.getenv("NAME", default="World")
 
 app = FastAPI()
- 
-# TODO: 1.) Add endpoint as a top-level page.
+
 @app.get("/")
 def index():
     return {"message": f"Hello {NAME}"}
 
-# TODO: 2.) Create a calculator that accept 2 query params and manipulate those 2 values.
+
 @app.get("/calculator/add")
-def adder(first_number: int, second_number: int):
+def adder(first_number: int, second_number: int) -> CalculatorResponse:
     # TODO: Start here
-    return {"first_number": first_number, "second_number": second_number, "result": first_number+second_number}
+    return {
+        "first_number": first_number,
+        "second_number": second_number,
+        "result": first_number + second_number,
+    }
+
 
 @app.get("/calculator/substract")
-def subtractor(first_number: int, second_number: int):
+def subtractor(first_number: int, second_number: int) -> CalculatorResponse:
     # TODO: Start here
-    return {"first_number": first_number, "second_number": second_number, "result": first_number-second_number}
+    return {
+        "first_number": first_number,
+        "second_number": second_number,
+        "result": first_number - second_number,
+    }
 
-# TODO: (Optional) @app.get("/calculator/multiply")
+
 @app.get("/calculator/multiply")
-def multiplier(first_number: int, second_number: int):
-    return {"first_number": first_number, "second_number": second_number, "result": first_number*second_number}
+def multiplier(first_number: int, second_number: int) -> CalculatorResponse:
+    return {
+        "first_number": first_number,
+        "second_number": second_number,
+        "result": first_number * second_number,
+    }
 
-# TODO: (Optional) @app.get("/calculator/divide")
-@app.get("/calculator/divide")
-def diviver(first_number: int,second_number: int):
-    if(second_number != 0):
-        return {"first_number": first_number, "second_number": second_number, "result": first_number/second_number}
+
+@app.get("/calculator/divide", responses={status.HTTP_400_BAD_REQUEST: {"model": Message}})
+def diviver(
+    first_number: int,
+    second_number: int,
+) -> CalculatorResponse:
+    if second_number != 0:
+        return {
+            "first_number": first_number,
+            "second_number": second_number,
+            "result": first_number / second_number,
+        }
     else:
-        return {"first_number": first_number, "second_number": second_number, "result": "zero division error"}
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, detail="Can't divide by zero"
+        )
 
 
-# TODO: 3.) Enter a path scheme that accept userId as a path parameter `userId`.
-@app.get("/userId")
-def usersInfo(userId: int):
+@app.get("/userId", responses={status.HTTP_404_NOT_FOUND: {"model": Message}})
+def usersInfo(userId: int) -> UserInfo:
     if userId != 9997:
-        raise HTTPException(status_code=404, detail="UserId not found.")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="UserId not found.")
 
     return {
         "id": userId,
@@ -53,15 +74,6 @@ def usersInfo(userId: int):
     }
 
 
-# TODO: 4.) Make this method to accept `POST` request and request body on path `/item/create/`.
-# Example Request Body:
-# {
-#   "name": "Book",
-#   "price": 123,
-# }
-@app.post("/item/create")
+@app.post("/item/create", status_code=status.HTTP_204_NO_CONTENT)
 def itemCreator(item: Item):
     print(f"name is {item.name} , price is {item.price}")
-    return item.name,item.price
-
-
